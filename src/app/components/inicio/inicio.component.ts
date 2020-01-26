7df48a58691030b9e5982935835f49c5ea2e395b
Leonardo Reyes <leonardo.reyes@colfuturo.org>
2020-01-26 09:00:04 -0500

    Consumo servicio nuevos lanzamientos

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styles: []
 })
-export class InicioComponent implements OnInit {
+export class InicioComponent {
 
-  constructor() { }
+  constructor(private spotify: SpotifyService) {
+    
+    this.spotify.getUltimosLanzamientos();
 
-  ngOnInit() {
-  }
+   }
+
+  
 
 }
---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http:HttpClient) { }
+
+  getUltimosLanzamientos(){
+    const headers = new HttpHeaders({
+      'Authorization':'Bearer BQBIF1aqJMjzp0STO8TlLhq2Lo9yPCqrnMgOjy6bl0pxfd3rO4C-4CCff3k2JOZpRpP5B2Y31Cf9dVCVQa0'
+    });
+    this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=10', { headers })
+        .subscribe(respuesta => {
+          console.log(respuesta)
+        });
+  }
+
+}
